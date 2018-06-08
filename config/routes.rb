6d2feeff9580edbb7680d7f8b12b7401969411cf
Michael Hirschberger <mlhirschberger@gmail.com>
2018-06-08 00:11:19 -0400

    Added home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :courses
   resources :universities
+  root 'application#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
