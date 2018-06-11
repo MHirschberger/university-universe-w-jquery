af4e75eabe55c0bf9050565161368e9602861480
Michael Hirschberger <mlhirschberger@gmail.com>
2018-06-10 23:46:42 -0400

    Updated user erb files and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :first_name, :last_name, :email, :username, :password, :password_confirmation, presence:true
+    validates :username, :email, uniqueness: true
 end
